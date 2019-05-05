1af4e27a48de296eb98fbc52d15bd6a8ef2c40a9
qcmiao1998 <qcmiao@hotmail.com>
2019-05-05 20:40:55 +0800

    1.0.1

--- YYNB/MainActivity.cs
+++ YYNB/MainActivity.cs
@@ -31,16 +31,46 @@ namespace YYNB
         private void ImageButton_Click(object sender, EventArgs e)
         {
             imageButton.Background = new ColorDrawable(new Color(rand.Next(255), rand.Next(255), rand.Next(255)));
-            using (var httpClient = new HttpClient())
-            {
-                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://busuanzi.ibruce.info/busuanzi?jsonpCallback=BusuanziCallback_854782526265"))
-                {
-                    request.Headers.TryAddWithoutValidation("Referer", "https://iamsmally.github.io/");
-                    request.Headers.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36");
-                    request.Headers.TryAddWithoutValidation("DNT", "1");
+            //using (var httpClient = new HttpClient())
+            //{
+            //    using (var request = new HttpRequestMessage(new HttpMethod("GET"), $"https://busuanzi.ibruce.info/busuanzi?jsonpCallback=BusuanziCallback_{Math.Floor(rand.NextDouble() * 1099511627776)}"))
+            //    {
+            //        request.Headers.TryAddWithoutValidation("Referer", "https://iamsmally.github.io/");
+            //        request.Headers.TryAddWithoutValidation("User-Agent", RandomUseragent());
+            //        request.Headers.TryAddWithoutValidation("DNT", "1");
 
-                    httpClient.SendAsync(request);
+            //        httpClient.SendAsync(request);
+            //    }
+            //}
+            SendGet();
+        }
+
+        private async void SendGet()
+        {
+            try
+            {
+                string re;
+                using (var httpClient = new HttpClient())
+                {
+                    using (var request = new HttpRequestMessage(new HttpMethod("GET"), $"https://busuanzi.ibruce.info/busuanzi?jsonpCallback=BusuanziCallback_{Math.Floor(rand.NextDouble() * 1099511627776)}"))
+                    {
+                        request.Headers.TryAddWithoutValidation("Referer", "https://iamsmally.github.io/");
+                        request.Headers.TryAddWithoutValidation("User-Agent", RandomUseragent());
+                        request.Headers.TryAddWithoutValidation("DNT", "1");
+
+                         using (HttpResponseMessage response = httpClient.SendAsync(request).Result)
+                         {
+                             re = await response.Content.ReadAsStringAsync();
+                         }
+                    }
                 }
+                TextView textView = FindViewById<TextView>(Resource.Id.textView1);
+                string rn = re.Split(',')[3].Split(':')[1].Split('}')[0];
+                textView.Text = $"YY has {rn} views!";
+            }
+            catch (Exception)
+            {
+                // ignored
             }
         }
 
@@ -73,6 +103,33 @@ namespace YYNB
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
-	}
+
+        private string RandomUseragent()
+        {
+            string[] usersagents = new string[]
+            {
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/22.0.1207.1 Safari/537.1",
+                "Mozilla/5.0 (X11; CrOS i686 2268.111.0) AppleWebKit/536.11 (KHTML, like Gecko) Chrome/20.0.1132.57 Safari/536.11",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1092.0 Safari/536.6",
+                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.6 (KHTML, like Gecko) Chrome/20.0.1090.0 Safari/536.6",
+                "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/19.77.34.5 Safari/537.1",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.9 Safari/536.5",
+                "Mozilla/5.0 (Windows NT 6.0) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.36 Safari/536.5",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_0) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1063.0 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1062.0 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.1 Safari/536.3",
+                "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/536.3 (KHTML, like Gecko) Chrome/19.0.1061.0 Safari/536.3",
+                "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
+                "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
+            };
+            var randomNumber = rand.Next(0, usersagents.Length);
+            return usersagents[randomNumber];
+        }
+}
 }
 
